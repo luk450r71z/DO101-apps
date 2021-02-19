4395c7ef54ed6b52c2bacaba61e500153228fc55
luk450r71z <lucasdjbg@gmail.com>
2021-02-19 11:24:41 -0300

    Se agrega mensaje alucivo a nuestro ministro

--- version/app.js
+++ version/app.js
@@ -6,8 +6,9 @@ var response;
 app.get('/', function (req, res) {
 
     //response = 'This is version 1 of the app.' + '\n';
-    response = 'This is version 2 of the app.' + '\n';
-
+    //response = 'This is version 2 of the app.' + '\n';
+    response = 'This is version 3 of the app.' + '\n
+    VIVA EL MINISTRO GONZALES  GUIÑO GUIÑO';
     //send the response to the client
     res.send(response);
 
